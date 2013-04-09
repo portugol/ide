d9c6a50fcd89bbc098d1c2c93aa57a925def8352
vpalmeirao <vascopalmeirao89@live.com.pt>
2013-04-09 22:20:14 +0100

    teste branch vasco

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ var Connection = function (r, source, target){
 };
 
 
-Raphael.fn.connection = function (obj1, obj2, line, bg) {
+Raphael.fn.connection = function (obj1  , obj2, line, bg) {
     if (obj1.line && obj1.from && obj1.to) {
         line = obj1;
         obj1 = line.from;
