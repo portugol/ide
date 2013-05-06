de1737001cb4ad92c7f24c0a0ddb2c7dcd62af15
jpgmauricio <jpgmauricio@gmail.com>
2013-05-06 23:26:02 +0100

    comit

---
+++ DragDrop/graph.js
@@ -0,0 +1,42 @@
+var Graph = function (r){
+    this.nodes = [];
+    this.lines = [];
+    this.r = r;
+    self = this;
+};
+
+Graph.prototype.add = function (node){
+
+    if (this.root === undefined) {
+        this.root = node;
+    }
+    this.nodes.push(node);
+};
+
+Graph.prototype.removeline = function(line){
+    for(var i = 0; i < this.lines.length;i++){
+        if(line == this.lines[i]){
+            this.lines.splice(i,1);
+        }
+    }
+}
+Graph.prototype.remove = function(node){
+    for(var i = 0; i < this.nodes.length; i++){
+        if(node == this.nodes[i]){
+            this.nodes.splice(i,1);
+        }
+    }
+}
+
+Graph.prototype.extract = function (){
+    for (var i = 0; i < this.nodes.length; i++) {
+        delete this.nodes[i].shape[1];
+        delete this.nodes[i].shape;
+        delete this.nodes[i].r;
+    };
+    for(var i = 0; i < this.lines.length; i++){
+        delete this.lines[i].shape;
+        delete this.lines[i].r;
+    };
+    return this;
+};
